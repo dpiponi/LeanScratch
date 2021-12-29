38ade87928ce67a8a7ba674a0cb32454a80494a7
Dan Piponi <dpiponi@gmail.com>
2021-12-29 09:50:25 -0800

    Removed useless imports

--- navigate.lean
+++ navigate.lean
@@ -1,33 +1,20 @@
 -- https://avigad.github.io/programming_in_lean/writing_tactics.html
 
-import init.meta.environment
-import init.meta.simp_tactic
-import control.basic
-import tactic.lint.basic
-import init.meta.tactic init.meta.rewrite_tactic init.meta.simp_tactic
-import init.meta.smt.congruence_closure
-import init.meta.interactive_base init.meta.derive init.meta.match_tactic
+--import tactic.find
 
-open lean
-open lean.parser
 
-open classical
-
-open environment
 open tactic
-open functor
-open monad
-open expr
 open tactic.interactive («have»)
-open tactic (get_local infer_type)
-open lean.parser (tk)
 open interactive (loc.ns)
 open interactive.types (texpr location)
 open interactive (parse)
 open lean.parser (ident)
 
 
-meta def tactic.interactive.mul_left (q : parse texpr) : parse location → tactic unit
+--#find (¬ _) → ¬ _
+
+meta def tactic.interactive.mul_left (q : parse texpr)
+  : parse location → tactic unit
 | (loc.ns [some h]) := do
    e ← tactic.i_to_expr q,
    H ← get_local h,
@@ -42,7 +29,8 @@ begin
   exact hyp
 end
 
-theorem descend_and_left {a b c : Prop} : (a → b) → a ∧ c → b ∧ c :=
+theorem descend_and_left {a b c : Prop}
+  : (a → b) → a ∧ c → b ∧ c :=
 begin
   intro h,
   intro ac,
@@ -52,28 +40,33 @@ begin
   apply and.right ac,
 end
 
-theorem descend_or_left {a b c : Prop} : (a → b) → a ∨ c → b ∨ c :=
+theorem descend_or_left {a b c : Prop}
+  : (a → b) → a ∨ c → b ∨ c :=
 assume h,
   assume ac,
     or.elim ac
       (assume ha, or.inl (h ha))
       (assume hc, or.inr hc)
 
-theorem descend_imp_left {a b c : Prop} : (a → b) → (b → c) → (a → c) :=
+theorem descend_imp_left {a b c : Prop}
+  : (a → b) → (b → c) → (a → c) :=
  assume f, assume g, g ∘ f
 
-theorem descend_imp_right {a b c : Prop} : (a → b) → (c → a) → (c → b) :=
+theorem descend_imp_right {a b c : Prop}
+  : (a → b) → (c → a) → (c → b) :=
  assume f, assume g, f ∘ g
  --function.comp
 
-theorem descend_or_right {a b c : Prop} : (a → b) → c ∨ a → c ∨ b :=
+theorem descend_or_right {a b c : Prop}
+  : (a → b) → c ∨ a → c ∨ b :=
 assume h,
   assume ac,
     or.elim ac
       (assume hc, or.inl hc)
       (assume ha, or.inr (h ha))
 
-theorem descend_and_right {a b c : Prop} : (a → b) → c ∧ a → c ∧ b :=
+theorem descend_and_right {a b c : Prop}
+  : (a → b) → c ∧ a → c ∧ b :=
 begin
   intro h,
   intro ca,
@@ -83,7 +76,8 @@ begin
   apply and.right ca,
 end
 
-theorem descend_not {a b c : Prop} : (a → b) → ¬ b → ¬ a :=
+theorem descend_not {a b c : Prop}
+  : (a → b) → ¬ b → ¬ a :=
 begin
   intro h,
   intro nb,
@@ -91,8 +85,8 @@ begin
   exact nb (h a)
 end
 
-example (a b c d e f : Prop) (hyp : b → a) :
-  (a → c) → (b → c) :=
+example (a b c d e f : Prop) (hyp : b → a)
+  : (a → c) → (b → c) :=
 begin
   intro adc,
   apply descend_imp_left,
@@ -101,8 +95,8 @@ begin
   apply hyp,  
 end
 
-example (a b c d e f : Prop) (hyp : a → b) :
-  (c → a) → (c → b) :=
+example (a b c d e f : Prop) (hyp : a → b)
+  : (c → a) → (c → b) :=
 begin
   intro adc,
   apply descend_imp_right,
@@ -112,7 +106,8 @@ begin
 end
 
 meta mutual def dobinary, dounary, dozoom
-with dobinary : expr → expr → expr → (name) → tactic name | u l e thm :=
+with dobinary : expr → expr → expr → (name) → tactic name
+| u l e thm :=
   unify u l >> (do
     applyc thm,
     swap,
@@ -122,7 +117,8 @@ with dobinary : expr → expr → expr → (name) → tactic name | u l e thm :=
     clear e,
     dozoom x hx)
 
-with dounary : expr → (name) → tactic name | e thm :=
+with dounary : expr → (name) → tactic name
+| e thm :=
   do
     applyc thm,
     swap,
@@ -137,37 +133,56 @@ with dozoom : name → expr → tactic name | q1 e :=
     t <- infer_type e >>= whnf,
     g <- tactic.target >>= whnf,
     match t with
-    | `(%%u ∧ %%v) := (do
-      `(%%l ∧ %%r) ← tactic.target >>= whnf,
-      dobinary u l e ``descend_and_right
+    | `(%%u ∧ %%v) := do
+        `(%%l ∧ %%r) ← tactic.target >>= whnf,
+        dobinary u l e ``descend_and_right
       <|> dobinary v r e ``descend_and_left
-      <|> return q1)
-    | `(%%u ∨ %%v) := (do
-      `(%%l ∨  %%r) ← tactic.target >>= whnf,
-      dobinary u l e ``descend_or_right
-      <|>
-      dobinary v r e ``descend_or_left
-      <|> return q1)
-    | `(%%u → %%v) := (do
-      `(%%l → %%r) ← tactic.target >>= whnf,
-      dobinary u l e ``descend_imp_right
-      <|>
-      dobinary v r e ``descend_imp_left
-      <|> return q1)
-| `(¬ %%u) := (do
-      `(¬ %%l) ← tactic.target >>= whnf,
-      dounary e ``descend_not
-      <|> return q1)
+      <|> return q1
+    | `(%%u ∨ %%v) := do
+        `(%%l ∨  %%r) ← tactic.target >>= whnf,
+        dobinary u l e ``descend_or_right
+      <|> dobinary v r e ``descend_or_left
+      <|> return q1
+    | `(%%u → %%v) := do
+        `(%%l → %%r) ← tactic.target >>= whnf,
+        dobinary u l e ``descend_imp_right
+      <|> dobinary v r e ``descend_imp_left
+      <|> return q1
+| `(¬ %%u) := do
+        `(¬ %%l) ← tactic.target >>= whnf,
+        dounary e `descend_not
+      <|> return q1
     | _ := return q1
     end
 
 meta def tactic.interactive.zoom (q1 : parse ident) (q : parse texpr) : tactic unit := do
   e ← tactic.i_to_expr q,
-  --q2 <- tactic.i_to_expr q1,
   n <- dozoom q1 e,
-  trace "n, q1 =",
-  trace (q1, n)
-  --rename q1 n
+  when (q1 ≠ n) $ rename n q1
+
+example (a b : Prop) (hyp : a → b) :
+  a → b :=
+begin
+  intro hac,
+  zoom z hac,
+  apply hyp, assumption,
+end
+
+example (a b : Prop) (hyp : a → b) :
+  ¬ b → ¬ a :=
+begin
+  intro hac,
+  zoom z hac,
+  apply hyp, assumption,
+end
+
+example (a b c : Prop) (hyp : a → b) :
+  a ∨ c → b ∨ c :=
+begin
+  intro hac,
+  zoom z hac,
+  apply hyp, assumption,
+end
 
 example (a b c d e f : Prop) (hyp : b → a) :
   (¬ (e ∧ (b → f)) ∧ d) ∧ c → (¬ (e ∧ (a → f)) ∧ d) ∧ c :=
@@ -175,4 +190,12 @@ begin
   intro hac,
   zoom z hac,
   apply hyp, assumption,
+end
+
+example (a b : Prop) (hyp : a → b) :
+  (∃ (i : ℕ), a) → ∃ (i : ℕ), b :=
+begin
+  intro hac,
+  zoom z hac, -- does nothing
+  cases hac with i p, existsi i, apply hyp, assumption
 end
